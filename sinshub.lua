bb864914dbe9409af94708f7317ea8636b3be408
SinshubCreator <92997478+SinshubCreator@users.noreply.github.com>
2021-10-22 21:38:12 +0300

    Update sinshub.lua

--- sinshub.lua
+++ sinshub.lua
@@ -1,6 +1,4 @@
-
-
-local loader = Instance.new("ScreenGui")
+ local loader = Instance.new("ScreenGui")
 local loader_2 = Instance.new("Frame")
 local UIGradient = Instance.new("UIGradient")
 local UICorner = Instance.new("UICorner")
@@ -20,10 +18,13 @@ load.MouseButton1Down:connect(function()
     if game.PlaceId == 6284583030 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/blxtok/SinsHub/main/petsimx.lua"))()
     elseif game.PlaceId == 621129760 then          -- buralara oyunid ve scriptleri yazcan
-         loadstring(game:HttpGet("https://raw.githubusercontent.com/ZinityDrops/OwlHubLink/master/OwlHubBack.lua"))();
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ZinityDrops/OwlHubLink/master/OwlHubBack.lua"))();
     elseif game.PlaceId == 734159876 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/alphaalt0409/WEIRDAPPLEBEEPANEL/main/weirdapplebee.lua"))()
+    
+        game.Players.LocalPlayer:Kick("Game not supported.")
     end
+    loader:Destroy()
 end)
 
 ----- burdan sonrasını elleme 
@@ -33,8 +34,9 @@ loader_2.Parent = loader
 loader_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 loader_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
 loader_2.BorderSizePixel = 0
-loader_2.Position = UDim2.new(0.509940684, 0, 0.498808324, 0)
-loader_2.Size = UDim2.new(0, 1, 0, 0)
+loader_2.Position = UDim2.new(0.368, 0,0.371, 0)
+loader_2.Size = UDim2.new(0, 335,0, 210)
+loader_2.Draggable = true
 
 UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(45, 168, 182)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(64, 96, 161))}
 UIGradient.Parent = loader_2
@@ -49,7 +51,7 @@ sins.BackgroundTransparency = 1.000
 sins.BorderColor3 = Color3.fromRGB(27, 42, 53)
 sins.Position = UDim2.new(0.0382089168, 0, -0.0333333351, 0)
 sins.Size = UDim2.new(0, 220, 0, 85)
-sins.Visible = false
+sins.Visible = true
 sins.Font = Enum.Font.GothamBlack
 sins.Text = "Sins"
 sins.TextColor3 = Color3.fromRGB(255, 0, 4)
@@ -61,7 +63,7 @@ hub.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 hub.BackgroundTransparency = 1.000
 hub.Position = UDim2.new(0.322073221, 0, -0.0333333351, 0)
 hub.Size = UDim2.new(0, 220, 0, 85)
-hub.Visible = false
+hub.Visible = true
 hub.Font = Enum.Font.GothamBlack
 hub.Text = "Hub"
 hub.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -72,7 +74,7 @@ load.Parent = loader_2
 load.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
 load.Position = UDim2.new(0.0304051656, 0, 0.332730263, 0)
 load.Size = UDim2.new(0, 313, 0, 62)
-load.Visible = false
+load.Visible = true
 load.Font = Enum.Font.Ubuntu
 load.Text = "Load"
 load.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -86,7 +88,7 @@ exit.Parent = loader_2
 exit.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
 exit.Position = UDim2.new(0.0304051656, 0, 0.646308959, 0)
 exit.Size = UDim2.new(0, 313, 0, 62)
-exit.Visible = false
+exit.Visible = true
 exit.Font = Enum.Font.Ubuntu
 exit.Text = "Exit"
 exit.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -100,8 +102,8 @@ UICorner_3.Parent = exit
 
 wait(2)
 
-script.Parent:TweenSize(UDim2.new(0, 335,0, 210),nil,nil,2)
-script.Parent:TweenPosition(UDim2.new(0.368, 0,0.371, 0),nil,nil,2)
+loader_2.Parent:TweenSize(UDim2.new(0, 335,0, 210),nil,nil,2)
+loader_2:TweenPosition(UDim2.new(0.368, 0,0.371, 0),nil,nil,2)
 
 wait(2)
 
