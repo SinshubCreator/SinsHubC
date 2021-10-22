98fc2836287f798a67399dc7aa594fb49fe68e28
SinshubCreator <92997478+SinshubCreator@users.noreply.github.com>
2021-10-23 01:52:21 +0300

    Update sinshub.lua

--- sinshub.lua
+++ sinshub.lua
@@ -27,6 +27,8 @@ load.MouseButton1Down:connect(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xennyy/Xenny-Ware/main/loader.lua"))()
     elseif game.PlaceId == 7065731541 then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AnonymousErrors/newscripts/main/main.lua"))()
+    elseif game.PlaceId == 1962086868 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SinshubCreator/Strongest-Punch-Simulator/main/towerofhell"))()
     end
     loader:Destroy()
 end)
