ad1b541de3cd59afccee64eb7dc217d35dcfb9af
SinshubCreator <92997478+SinshubCreator@users.noreply.github.com>
2021-12-26 02:29:32 +0300

    Update sinshub.lua

--- sinshub.lua
+++ sinshub.lua
@@ -114,8 +114,10 @@ load.MouseButton1Down:connect(function()
                  loadstring(game:HttpGet"https://pastebinp.com/raw/eccv9pgu")()
         elseif game.PlaceId == 815405518 then
                  loadstring(game:GetObjects("rbxassetid://1683559539")[1].Source)()
-        elseif game.PlaceId == then
+        elseif game.PlaceId == 488667523 then
                  loadstring(game:HttpGet("https://raw.githubusercontent.com/GwnStefano/NexusHub/main/Main", true))()
+        elseif game.PlaceId == 4872321990 then
+                 loadstring(game:HttpGet("http://void-scripts.com/Scripts/islands_new.lua"))()
 	end
 	loader:Destroy()
 end)
