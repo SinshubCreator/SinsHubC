86637332fedb09a6c40676da3a9e6009e5eff684
SinshubCreator <92997478+SinshubCreator@users.noreply.github.com>
2021-12-31 01:57:29 +0300

    Update sinshub.lua

--- sinshub.lua
+++ sinshub.lua
@@ -178,6 +178,8 @@ load.MouseButton1Down:connect(function()
                   loadstring(game:HttpGet(('https://raw.githubusercontent.com/kilav3rm/Scripts/main/MeepCity'),true))()
         elseif game.PlaceId == 606849621 then
                   loadstring(game:HttpGet("https://raw.githubusercontent.com/KuriWasTaken/MonkeyScripts/main/JailMonkey.lua"))()
+        elseif game.PlaceId == 6769764667 then
+                  loadstring(game:HttpGet("https://raw.githubusercontent.com/Xowie89/Roblox-Scripts/main/Factory%20Simulator%20%5BOVERHAUL%5D.lua"))()
 
 	end
 	loader:Destroy()
